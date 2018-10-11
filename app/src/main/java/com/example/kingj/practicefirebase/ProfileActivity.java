19307abccca4ba92d6302aa3c1e9c6a9b1b84d5d
jasmeet <jasmeet2109@outlook.com>
2018-10-11 15:20:53 +0530

    crashlytics

--- app/src/main/java/com/example/kingj/practicefirebase/ProfileActivity.java
+++ app/src/main/java/com/example/kingj/practicefirebase/ProfileActivity.java
@@ -2,6 +2,7 @@ package com.example.kingj.practicefirebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        int a=4;
+        int b=0;
+        TextView textView;
+        textView = findViewById(R.id.textView);
+//        int c =a/b;
+//        textView.setText(""+c);
     }
 }
